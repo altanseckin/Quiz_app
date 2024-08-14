a0c4ef1df1e972be7e9c636d1d9f5283bfde36dc
Seçkin Soylu <seckin.soylu@exagate.com>
2024-08-15 00:07:09 +0300

    dump

--- quiz_app_project/settings.py
+++ quiz_app_project/settings.py
@@ -15,11 +15,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv,find_dotenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(find_dotenv())
-#print(os.getenv('EMAIL_HOST_USER'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
